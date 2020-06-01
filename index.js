6720bd6a4004c86298bcfdd3eb269ce98154e3d9
aidanmc95 <aidanmc95@gmail.com>
2020-06-01 14:14:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
+document.body.appendChild(newHeader);
+console.dir(newHeader);
