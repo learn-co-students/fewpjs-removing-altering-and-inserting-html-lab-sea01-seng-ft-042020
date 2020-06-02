cf703af39bd85bdbe5cd8b4a6e759ea6acde6793
Trevor <tlow94@uw.edu>
2020-06-02 16:31:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+
+var main = document.querySelector('main');
+main.remove()
+
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Trevor is the champion"
+
+
+//   it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+//     expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+//   });
